5ea5af9ccaa73e9b1e9fc1bf695faeb5f1bbfe11
GramescuDan <gramescudan@yahoo.com>
2021-10-31 01:10:46 +0300

    fix:setting maxlength

--- WebPage.Domain/Abstractions/Article.cs
+++ WebPage.Domain/Abstractions/Article.cs
@@ -8,6 +8,8 @@ namespace WebPage.Domain.Abstractions
 {
     class Article
     {
+        public int MaxLengthTitle = 0;
+        public int MaxLengthDescription = 0;
         public string Title { get; set; }
         public string Description { get; set; }
     }
