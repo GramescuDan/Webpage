3973af100cc581a18b9ae450854f0c34dd927803
GramescuDan <gramescudan@yahoo.com>
2021-11-01 22:26:56 +0200

    fix: almost working dbcontext

---
+++ WebPage.DAL/Database/DbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.ApplicationServices;
+using WebPage.Domain.Models;
+
+namespace WebPage.DAL.Database
+{
+    public class DbContext: IdentityDbContext<User,Role,string>
+    {
+        public DbContext(DbContextOptions<DbContext> options): base(options)
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Article> Articles; 
+    }
+}
